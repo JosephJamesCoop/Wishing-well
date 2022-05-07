93f0d052c6fb91fc19902b337ce2b7b6deaee0f2
JosephJamesCoop <cooper_3869@live.com>
2022-05-07 13:07:16 -0700

    long convo added

--- src/components/pages/LongConvo/index.js
+++ src/components/pages/LongConvo/index.js
@@ -6,7 +6,53 @@ function LongConvo() {
       <div class="my-info">
         <h3>First Long Conversation:</h3>
         <p className="paragraph">
-
+          Well after spending months and months standing in the aisle in front
+          of my department "waiting for customers". In reality I was very
+          strategically poisitioning myself so I would always face customer
+          service, especially whenever another employee would approach to tell
+          me something usually of no value at all. Word was going around the
+          store that my Supervisor Jessy was going to have a get together for
+          his and another co-workers birthday at a local bar. I wanted to go but
+          at the same time a night at the bar surrounded by drunks did not sound
+          too exciting until... It was CONFIRMED! Erin was going to go, then my
+          entire perspective on a night at the bar changed! I was like yeah
+          Jessy I don't care if I'm invited or not I'll see you friday night!
+          Friday night came by and I was ready to go, showed up with my buddy
+          Jason and we were there to PARTY! We hung out said hi to everyone and
+          then evntually made our way up stairs where I laid my eyes on her. All
+          my confidence and energy went out the window and I imediatly shut
+          down. Intimidated by her beauty and covered in tattoos with holes in
+          her ears 3 times the size of mine. I lost all confidence and stuck my
+          tail between my legs and sat down on the other side of the room from
+          her. About 1 hour 7 minues and 35 seconds later, again it was about
+          that much time later I don't know for sure but I think it was about 1
+          hour 7 minutes and 35 seconds later. A lady with about 40 roses came
+          up stairs and was selling them for $5 each, I was like hell yeah,
+          bought one and was imidiatly judged and questioned why I just wasted
+          my money on a flower and who it was for. Under a lot of pressure and
+          still not enough confidence to give it to Erin I did what most people
+          do in a panic, I LIED! I lied hard and I lied good, my inner salesman
+          came out and I sold the lie to a crowd of salesman and they bought it!
+          It was for me, DUH! What a dude can't buy himself nice things to feel
+          pretty and special? Maybe I'm going to take it home and plant a rose
+          bed. Or maybe I am going to take it home and tell my roomates the
+          cutest girl ever gave it to me! I don't know I haven't decided yet but
+          you know what I do know, I just spent $5 on a world of opportunites
+          that none of you will get to experience because you were to afraid to
+          make the investment. Okay maybe they didn't buy it but they laughed
+          pretty hard and it boosted my confidence. Thats when I noticed Erin's
+          friend Danielle was no longer sitting next to her and I finally got
+          some confidence and sat next to her with the rose in my hand.
+          Something about my rear end connecting with the seat or maybe just the
+          way I was sitting but literally all of my confidence went out the
+          window once again. I started with small talk after a few hours of
+          talking I learned she is beautiful on the outside and inside and also
+          likes spongebob HUGE plus. But I also found out she has a boyfriend... When it was time for us to leave Danielles boyfriend Mitch
+          came up and was like hey someone just pulled a knife on someone in the
+          parking lot do you need me to walk you out Erin? I looked over at
+          Mitch and said oh I got this, I can walk her out he smiled back at me
+          and Erin said okay. So I walked her to her car said goodbye and tossed
+          her the rose on the passenger seat and watched her drive away.
         </p>
       </div>
     </>
