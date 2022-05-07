99b4c5656244e661fc2b240512243fbcf1182d44
JosephJamesCoop <cooper_3869@live.com>
2022-05-07 13:06:56 -0700

    Seal the Deal added

---
+++ src/components/pages/FirstDate/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function FirstDate() {
+  return (
+    <>
+      <div class="my-info">
+        <h3>Our Official First Date:</h3>
+        <p className="paragraph">
+          
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default FirstDate;
---
+++ src/components/pages/LongConvo/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function LongConvo() {
+  return (
+    <>
+      <div class="my-info">
+        <h3>First Long Conversation:</h3>
+        <p className="paragraph">
+
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default LongConvo;
