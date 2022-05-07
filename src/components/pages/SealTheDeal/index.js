4b0da29065a3a20a0fede823d40293daada18f6a
JosephJamesCoop <cooper_3869@live.com>
2022-05-07 13:42:45 -0700

    updated some spelling

--- src/components/pages/SealTheDeal/index.js
+++ src/components/pages/SealTheDeal/index.js
@@ -11,36 +11,38 @@ function SealTheDeal() {
           the two coolest dudes in town and a dog. Like what else do you need to
           add to the equations for an epic party!? Apparently a lot more cause
           no one showed up even though like 70 people claimed to be on their way
-          lol. Out of desperation Jason and I searched many socail media
+          lol. Out of desperation Jason and I searched many social media
           platforms attempting to steal people from one party and get them to
           ours. With hours and hours of no success we about gave up, then Jason
           very casually says looks like Erin is home alone, even though he knows
-          I am like head over heels for Erin at this point. I immediatly beg him
-          to invite her and he is like nah bro she is with Taylor and I'm like
-          BRO she literally is saying boys suck, home alone, sad blahhh blahh
-          blahh. To which Jason responded well if she is home alone she probably
-          wants to be, took everything in me not to punch him... I was like we
-          are home alone and we don't want to be so you never know now invite
-          her. He does small talk and then eventually invites her over and she
-          says yes. I am so pumped on life at this point! Once Erin arrives she
-          is obviously sad and not happy so I sit down and start talking to her
-          attempting to find out what is wrong and how to use my great sense of
-          humor and my immaculate timing of jokes to cheer her up. After talking
-          for a little while I find out this Taylor guy she is dating is kind of
-          a giant piece of shit! But in my mind all I heard was "I am not happy
-          in my current relationship so I'm pretty much single so you wouldn't
-          but a jerk for trying to win my heart over and getting me to leave
-          taylor for you". So I gave it my best, I reassured her of her value
-          and right when I started to cheer her up, Jason's extremely drunk ass
-          came on over and attemtped to cheer her up by being suepr creepy and
-          telling her the same thing over and over again and then started being
-          a HUGE cock block! But because I was a nice guy and Jason was being a
-          creepy drunk at the moment it actually super worked in my favor and
-          snagged her number. Come to find out we both have the same days off
-          and she had no plans. So we made plans to go on a hike together the
-          next day and after seeing her in her chacos thats when I fell in love.
-          Not really, the chacos were almost a deal breaker but the time, laughs
-          and company she provided made me fall in love.
+          I am like head over heels for Erin at this point. I immediately beg
+          him to invite her and he is like nah bro she is with Taylor and I'm
+          like BRO she literally is saying boys suck, home alone, sad blahhh
+          blahh blahh. To which Jason responded well if she is home alone she
+          probably wants to be, took everything in me not to punch him... I was
+          like we are home alone and we don't want to be so you never know now
+          invite her. He does small talk and then eventually invites her over
+          and she says yes. I am so pumped on life at this point! Once Erin
+          arrives she is obviously sad and not happy so I sit down and start
+          talking to her attempting to find out what is wrong and how to use my
+          great sense of humor and my immaculate timing of jokes to cheer her
+          up. After talking for a little while I found out this Taylor guy she
+          is dating is not a stand up guy in the slightest bit! But in my mind
+          all I heard was "I am not happy in my current relationship so I'm
+          pretty much single so you wouldn't but a jerk for trying to win my
+          heart over and getting me to leave taylor for you". So I gave it my
+          best, I reassured her of her value and right when I started to cheer
+          her up, Jason being 6 or 7 more beers in than he should of been came
+          on over and attempted to cheer her up by being super creepy and
+          telling her the same thing over and over again. Jason being too drunk
+          to realize we were starting to have a moment started to ruin all my
+          chances. But because I was a nice guy and Jason was being a creepy
+          drunk at the moment it actually super worked in my favor and I snagged
+          her number. Come to find out we both have the same days off and she
+          had no plans. So we made plans to go on a hike together the next day
+          and after seeing her in her chacos that's when I fell in love. Not
+          really, the chacos were almost a deal breaker but the time, laughs and
+          company she provided made me fall in love.
         </p>
       </div>
     </>
