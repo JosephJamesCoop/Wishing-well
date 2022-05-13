65059531193685beddd2425f0f929f8790c6510c
JosephJamesCoop <cooper_3869@live.com>
2022-05-13 07:08:20 -0700

    filler for 2 paragraphs

--- src/components/pages/FirstDate/index.js
+++ src/components/pages/FirstDate/index.js
@@ -6,7 +6,7 @@ function FirstDate() {
       <div class="my-info">
         <h3>Our Official First Date:</h3>
         <p className="paragraph">
-          
+          You know how weddings take a lot of planning... This will be updated later...
         </p>
       </div>
     </>
--- src/components/pages/Proposal/index.js
+++ src/components/pages/Proposal/index.js
@@ -6,7 +6,7 @@ function Proposal() {
       <div class="my-info">
         <h3>The Big Proposal:</h3>
         <p className="paragraph">
-
+        You know how weddings take a lot of planning... This will be updated later...
         </p>
       </div>
     </>
